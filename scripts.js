3f6662ab59ccd8962158b84050f5f165831983ae
Jeannie <jntsao@gmail.com>
2021-05-21 23:52:52 -0700

    created functions for crossing out and deleting

--- scripts.js
+++ scripts.js
@@ -3,4 +3,35 @@ function newItem(){
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
+
+    if(inputValue === ''){
+        alert('You must write something!');
+    } else{
+        let list = $('#list');
+        list.append(li);
+    }
+
+    //cross out an item from the list of items
+    function crossOut(){
+        li.toggleClass('strike');
+    }
+
+    li.on("dblclick", function crossout(){
+        li.toggleClass('strike');
+    });
+
+    //adding delete button
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem(){
+        li.addClass('delete');
+    }
+
+    //reorder items
+    $('#list').sortable();
+
 }
